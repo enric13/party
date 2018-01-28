1b351fbaf71f607e68f07e290c54a435ba748b25
Enric Calabuig <ecalabuig@bmat.com>
2018-01-28 18:54:24 +0100

    Played around with routes and styles

--- src/app.py
+++ src/app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Ola, ke ase!'
+    return redirect(url_for('home'))
+
+@app.route('/home')
+def home():
+    return render_template('home.html', msg='WELCOME')
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
